a912606c07cd94f502ecb8862c9d12ac58de502d
victormln <victormln@users.noreply.github.com>
2019-08-27 22:49:20 +0200

    testing declaration of diferent type of variables

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import "fmt"
 
+const insertNameMessage = "Insert your name:"
+const insertAgeMessage = "How old are you?:"
+const welcomeMessage string = "Welcome %s!"
+
 func main() {
-  var name string
-  fmt.Print("Insert your name:")
-  fmt.Scanf("%s", &name)
-  fmt.Printf("Welcome %s!", name)
+	var name string
+	name = "Default name"
+	var age = 0
+	withoutType := "Variable is set but without setting the type :)"
+	fmt.Print(insertNameMessage)
+	fmt.Scanf("%s", &name)
+	fmt.Printf(welcomeMessage, name)
+	fmt.Print(insertAgeMessage)
+	fmt.Scanf("%d", &age)
+	fmt.Println(age, withoutType)
 }
