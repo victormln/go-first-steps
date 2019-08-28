4474cc09b34ced782fe09edc7dd773564778b5d6
victormln <victormln@users.noreply.github.com>
2019-08-29 00:17:46 +0200

    Learning loops and strings methods

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const insertNameMessage = "Insert your name: "
 const insertAgeMessage = "\nHow old are you?: "
@@ -28,6 +31,16 @@ func main() {
 	var text = "testing"
 	fmt.Println("Extract letter from", text, "(", string("testing"[1]), ")")
 	fmt.Println(len(string("testing")))
+
+	getArray()
+
+	getSlice()
+
+	ifTest()
+
+	forTest()
+
+	strings2()
 }
 
 func getName() string {
@@ -60,6 +73,82 @@ func getFloat() (float32, float64) {
 	return float32(0.1), float64(float32(0.1))
 }
 
+func getArray() {
+	var typeOfCar [2]string
+	randomNumbers := [3]int{5, 3, 12}
+	fmt.Println(randomNumbers)
+	typeOfCar[0] = "electric"
+	typeOfCar[1] = "gasoil"
+	fmt.Println(typeOfCar)
+}
+
+func getSlice() {
+	var slice []string
+	slice = append(slice, "hola", "que", "tal")
+	fmt.Println(slice)
+}
+
 func sum(number1 int, number2 int) int {
 	return number1 + number2
+}
+
+func ifTest() {
+	var number = 0
+	fmt.Println("Ingresa un numero del 1 al 10")
+	fmt.Scanf("%d", &number)
+	if number%2 == 0 {
+		fmt.Println("Numero par")
+	} else {
+		fmt.Println("Numero impar")
+	}
+
+	if number2 := 3; number2 == 3 {
+		fmt.Println("Dentro!")
+	}
+}
+
+func forTest() {
+	standardFor()
+
+	loopWithoutInitializeIntoit()
+
+	infiniteLoopWithBreak()
+}
+
+func standardFor() {
+	for counter := 0; counter < 3; counter++ {
+		fmt.Print(counter, ", ")
+	}
+	fmt.Println()
+}
+
+func loopWithoutInitializeIntoit() {
+	c := 6
+	for ; c > 0; c = c - 2 {
+		fmt.Print(c, " ")
+	}
+}
+
+func infiniteLoopWithBreak() {
+	fmt.Println()
+	s := 1000
+	for {
+		s -= 1000
+		if s == 0 {
+			fmt.Println("Ha acabado el for")
+			break
+		}
+	}
+}
+
+func strings2() {
+	message := "Hoooola, que tal, no paras de decir y tal y tal?"
+	fmt.Println(strings.ToUpper(message))
+	fmt.Println(strings.ToLower(message))
+	fmt.Println(strings.Replace(message, "tal", "ase", 0))
+	fmt.Println(strings.Replace(message, "tal", "ase", 1))
+	fmt.Println(strings.Replace(message, "tal", "ase", -1))
+	messageInArray := strings.Split(message, ",")
+	fmt.Println(messageInArray)
+	fmt.Println("Array length: ", len(messageInArray))
 }
